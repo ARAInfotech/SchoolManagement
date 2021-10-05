17dee121517d3f25a35113c22faad007c18b83f1
tomcyphilipkora <tomcyphilip567@gmaiol.com>
2021-10-05 14:34:21 +0530

    file management

---
+++ Project.WebAPI/FileManagement/Model/FileMngtModel.cs
@@ -0,0 +1,49 @@
+﻿#region NameSpace
+using System.IO;
+#endregion
+
+namespace FileManagement.Model
+{
+    #region FileMngtModel
+    /// <summary>
+    /// FileMngtModel
+    /// </summary>
+    public class FileMngtModel
+    {
+        #region FilePath
+        /// <summary>
+        /// FilePath
+        /// </summary>
+        public string FilePath { get; set; }
+        #endregion
+
+        #region FileName
+        /// <summary>
+        /// FileName
+        /// </summary>
+        public string FileName { get; set; }
+        #endregion
+
+        #region ExisingFileName
+        /// <summary>
+        /// ExisingFileName used for replace an existing file with new
+        /// </summary>
+        public string ExisingFileName { get; set; }
+        #endregion
+
+        #region FileType
+        /// <summary>
+        /// FileType
+        /// </summary>
+        public string FileType { get; set; }
+        #endregion
+
+        #region FileContent
+        /// <summary>
+        /// FileContent
+        /// </summary>
+        public MemoryStream FileContent { get; set; }
+        #endregion
+    }
+    #endregion
+}
